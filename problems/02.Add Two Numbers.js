ec4b72db9275bee81a78c5b5994cca55f7df4ac3
Anshiii <bisexua1@foxmail.com>
2018-10-02 09:10:21 +0800

    add 02

---
+++ problems/02.Add Two Numbers.js
@@ -0,0 +1,74 @@
+/**
+ * Created by Anshi on 2018/10/1.
+ */
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
+/*算术器 next有值或者flag有值都要继续算。*/
+
+/*这种困境： 根节点和普通节点 */
+var addTwoNumbers = function (l1, l2) {
+  let flag = 0;
+
+  function add(n1, n2,) {
+	let node = null;
+	if (n1 && n2) {
+	  let perItem = n1.val + n2.val + flag;
+	  let val = perItem % 10;
+	  flag = Math.floor(perItem / 10);
+	  node = new ListNode(val);
+	  node.next = add(n1.next, n2.next);
+	  return node;
+	} else if (n2) {
+	  let perItem = n2.val + flag;
+	  let val = perItem % 10;
+	  flag = Math.floor(perItem / 10);
+	  node = new ListNode(val);
+	  node.next = add(null, n2.next);
+	  return node;
+	} else if (n1) {
+	  let perItem = n1.val + flag;
+	  let val = perItem % 10;
+	  flag = Math.floor(perItem / 10);
+	  node = new ListNode(val);
+	  node.next = add(n1.next, null);
+	  return node;
+	} else if (flag) {
+	  node = new ListNode(flag);
+	  return node;
+	}
+	return node
+
+  }
+
+  let result = add(l1, l2);
+  console.log(result)
+  return result;
+};
+let l1 = new ListNode(1);
+
+l1.next = new ListNode(2); //21
+
+let l2 = new ListNode(3);
+
+l2.next = new ListNode(5); //53
+
+
+addTwoNumbers(l1, l2)

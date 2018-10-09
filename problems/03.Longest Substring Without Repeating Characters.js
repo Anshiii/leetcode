839ad6d43b4bdd536b484cc18063ffc8b2d811d5
Anshiii <bisexua1@foxmail.com>
2018-10-09 18:38:25 +0800

    add 03 添加哈希解法。

---
+++ problems/03.Longest Substring Without Repeating Characters.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Anshi on 2018/10/2.
+ */
+
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+/*无重复字符串，map 设定。*/
+
+/*可以把指定 index 之前的数据删除 数组。*/
+var lengthOfLongestSubstring = function (s) {
+  let map = {};
+  /*最小值是1,*/
+  let result = 0;
+
+  let start = 0;
+  for (let i = 0; i < s.length; i++) {
+	let str = s[i];
+	if (i === s.length - 1) {
+	  if (map[str] >= start) {
+		result = Math.max(result, i - start);
+	  } else {
+		result = Math.max(result, i - start + 1);
+	  }
+	  break;
+	}
+	if (map[str] >= start) {
+	  result = Math.max(result, i - start);
+	  /*遇到相同的。*/
+	  start = map[str] + 1;
+	}
+	map[str] = i;
+  }
+  return result
+};
+
+lengthOfLongestSubstring('pwwkew')
+lengthOfLongestSubstring('abadc')
+lengthOfLongestSubstring('aaaaaa')
+lengthOfLongestSubstring(' ')
+lengthOfLongestSubstring('')

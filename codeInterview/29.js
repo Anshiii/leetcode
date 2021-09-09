b32ff9e306b2427eb4f217ec197226a4ddb0ed33
Anshiii <bisexua1@foxmail.com>
2021-09-09 15:09:27 +0800

    add: 打印矩阵

--- codeInterview/29.js
+++ codeInterview/29.js
@@ -10,7 +10,81 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
- var spiralOrder = function(matrix) {
-    let m = matrix.length,n = matrix[0].length;
-    
+var spiralOrder = function (matrix) {
+  let m = matrix.length;
+  if (!m) return [];
+  let n = matrix[0].length,
+    count = 1,
+    result = [],
+    i = 0,
+    j = 0;
+  if (!m || !n) return result;
+
+  let ms = 0,
+    me = m - 1,
+    ns = 0,
+    ne = n - 1;
+  while (result.length < m * n) {
+    let mod = count % 4; // 0 1 2 3
+
+    switch (mod) {
+      case 0:
+        for (i = me; i >= ms; i--) {
+          result.push(matrix[i][j]);
+        }
+        i++;
+        ns++;
+        count++;
+        continue;
+      case 1:
+        for (j = ns; j <= ne; j++) {
+          result.push(matrix[i][j]);
+        }
+        j--;
+        ms++;
+        count++;
+
+        continue;
+      case 2:
+        for (i = ms; i <= me; i++) {
+          result.push(matrix[i][j]);
+        }
+        i--;
+        ne--;
+        count++;
+        continue;
+      case 3:
+        for (j = ne; j >= ns; j--) {
+          result.push(matrix[i][j]);
+        }
+        j++;
+        me--;
+
+        count++;
+
+        continue;
+    }
+  }
+  return result;
 };
+
+console.log(spiralOrder([[]]));
+console.log(spiralOrder([]));
+console.log(spiralOrder([[1]]));
+console.log(spiralOrder([[12, 3, 4]]));
+console.log(spiralOrder([[1], [2], [3]]));
+console.log(
+  spiralOrder([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+  ])
+);
+
+console.log(
+  spiralOrder([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ])
+);
